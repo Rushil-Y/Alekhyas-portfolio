f04eacfa1b2016cc4b96e89f8ef1b96f9d0d6a07
Rushil-Y <rushilreddy3@gmail.com>
2026-02-06 20:09:40 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Alekhyas Portfolio - Coming Soon!</h1>
+    </div>
+  );
+}
+
+export default App;
